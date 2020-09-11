73326a3f8f23f8c68ed807d27131ea11dc3448df
rpampush <rpampush@gaig.com>
2020-09-11 17:54:22 -0400

    ubuntu.

--- EasyMode/install.sh
+++ EasyMode/install.sh
@@ -13,8 +13,24 @@ sudo useradd dockeruser
 echo ""
 echo "Installing docker-ce"
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sudo sh get-docker.sh
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=arm64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker dockeruser
 
