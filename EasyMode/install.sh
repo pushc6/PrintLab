6c59ac9cc837c99ef05ce5dcf87e7322ff132b46
Ryan <ryan.pampush@gmail.com>
2020-09-12 11:38:15 -0400

    Update install.sh

--- EasyMode/install.sh
+++ EasyMode/install.sh
@@ -90,7 +90,7 @@ echo ""
 echo "Installing PrintLab alpha 1"
 cd /usr/local
 echo "Cloning PrintLab"
-sudo git clone http://github.com/pushc6/PrintLab.git
+sudo git clone http://github.com/pushc6/dev/PrintLab.git
 cd PrintLab
 
 sudo su - dockeruser -c "docker-compose up -d"
