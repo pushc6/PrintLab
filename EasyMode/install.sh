582f55fb1502c2954fcd55a985a5cd13cd8a0f1d
Ryan <ryan.pampush@gmail.com>
2020-10-04 15:07:38 -0400

    Update install.sh

--- EasyMode/install.sh
+++ EasyMode/install.sh
@@ -68,36 +68,36 @@ sudo apt-get install docker-compose -y
 
 echo ""
 echo "Installing WireGuard"
-add-apt-repository -y ppa:wireguard/wireguard
-apt-get update
-apt-get install -y wireguard
+sudo add-apt-repository -y ppa:wireguard/wireguard
+sudo apt-get update
+sudo apt-get install -y wireguard
 
 # Remove dnsmasq because it will run inside the container.
-apt-get remove -y dnsmasq
+sudo apt-get remove -y dnsmasq
 
 # Disable systemd-resolved if it blocks port 53.
-systemctl disable systemd-resolved
-systemctl stop systemd-resolved
+sudo systemctl disable systemd-resolved
+sudo systemctl stop systemd-resolved
 
 # Set DNS server.
-echo nameserver 1.1.1.1 >/etc/resolv.conf
+sudo echo nameserver 1.1.1.1 >/etc/resolv.conf
 
 # Load modules.
-modprobe wireguard
-modprobe iptable_nat
-modprobe ip6table_nat
+sudo modprobe wireguard
+sudo modprobe iptable_nat
+sudo modprobe ip6table_nat
 
 # Enable modules when rebooting.
-echo "wireguard" > /etc/modules-load.d/wireguard.conf
-echo "iptable_nat" > /etc/modules-load.d/iptable_nat.conf
-echo "ip6table_nat" > /etc/modules-load.d/ip6table_nat.conf
+sudo echo "wireguard" > /etc/modules-load.d/wireguard.conf
+sudo echo "iptable_nat" > /etc/modules-load.d/iptable_nat.conf
+sudo echo "ip6table_nat" > /etc/modules-load.d/ip6table_nat.conf
 
 # Check if systemd-modules-load service is active.
-systemctl status systemd-modules-load.service
+sudo systemctl status systemd-modules-load.service
 
 # Enable IP forwarding.
-sysctl -w net.ipv4.ip_forward=1
-sysctl -w net.ipv6.conf.all.forwarding=1
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo sysctl -w net.ipv6.conf.all.forwarding=1
 
 echo ""
 
