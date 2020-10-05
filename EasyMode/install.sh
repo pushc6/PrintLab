625fd69e06b77863ebc3c0e440414e9fcfdebff6
Ryan <ryan.pampush@gmail.com>
2020-10-05 10:33:51 -0400

    Update install.sh

--- EasyMode/install.sh
+++ EasyMode/install.sh
@@ -117,7 +117,7 @@ cd PrintLab
 sudo git checkout dev
 sudo git pull -r
 
-export VPNIP=$(read -p "What is the public IP or dynamic DNS for this VPN server?)
+read -p "What is the public IP or dynamic DNS for this VPN server? VPNIP; export VPNIP
 #read -p "What is the public IP or dynamic DNS for this VPN server? " vpnip
 #export $vpnip
 
