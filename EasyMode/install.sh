73b5ce247bf3f85643748610040247f686863aa5
Ryan <ryan.pampush@gmail.com>
2020-10-04 21:46:47 -0400

    Update install.sh

--- EasyMode/install.sh
+++ EasyMode/install.sh
@@ -93,14 +93,8 @@ sudo sh -c "echo "iptable_nat" > /etc/modules-load.d/iptable_nat.conf"
 sudo sh -c "echo "ip6table_nat" > /etc/modules-load.d/ip6table_nat.conf"
 
 #Creating wg0.conf
-sudo sh -c "cd /etc/wireguard"
-sudo sh -c "umask 077; wg genkey | tee privatekey | wg pubkey > publickey"
-sudo sh -c "echo [Interface] >> /etc/wireguard/wg0.conf"
-sudo sh -c "echo Address = 10.38.20.1/24 >> /etc/wireguard/wg0.conf"
-sudo sh -c "echo ListenPort = 51928 >> /etc/wireguard/wg0.conf"
-sudo sh -c "privkey=$(cat privatekey)"
-sudo sh -c "echo privkey is $privkey"
-sudo sh -c "echo PrivateKey = $privkey >> /etc/wireguard/wg0.conf"
+sudo sh -c "cd /etc/wireguard; umask 077; wg genkey | tee privatekey | wg pubkey > publickey; echo [Interface] >> /etc/wireguard/wg0.conf; echo Address = 10.38.20.1/24 >> /etc/wireguard/wg0.conf;
+echo ListenPort = 51928 >> /etc/wireguard/wg0.conf; privkey=$(cat privatekey); echo privkey is $privkey; echo PrivateKey = $privkey >> /etc/wireguard/wg0.conf"
 
 # Check if systemd-modules-load service is active.
 #sudo systemctl status systemd-modules-load.service
