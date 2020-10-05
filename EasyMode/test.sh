d0ba219456f646eb157c27f982d4e6d03e1f454f
Ryan <ryan.pampush@gmail.com>
2020-10-05 11:26:25 -0400

    Update test.sh

--- EasyMode/test.sh
+++ EasyMode/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 read -p "test? " ISYES
-echo " $ISYES is yes?"
+#echo " $ISYES is yes?"
 echo "VPNIP=$ISYES" > /tmp/vpnip.txt
 #sudo bash -c "echo VPINIP=$ISYES > test.txt"
 #$(sudo echo $ISYES) > test.txt"
-echo $ISYES
+#echo $ISYES
