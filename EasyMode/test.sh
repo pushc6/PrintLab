0652a89c9f41eb4f678062e08b8028fd3ada5181
Ryan <ryan.pampush@gmail.com>
2020-10-05 12:32:23 -0400

    Update test.sh

--- EasyMode/test.sh
+++ EasyMode/test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-read -p "test? " ISYES
-#echo " $ISYES is yes?"
-echo "VPNIP=$ISYES" > /tmp/vpnip.txt
-#sudo bash -c "echo VPINIP=$ISYES > test.txt"
-#$(sudo echo $ISYES) > test.txt"
-#echo $ISYES
+HTTPHOST=hostname -I | awk '{print $1}'
+
+echo $HTTPHOST
